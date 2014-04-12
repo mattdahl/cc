97442ca9ef0f96fb07ea22d53ac9d4f69f7df54f
Matt Dahl <matt.dahl.2013@gmail.com>
2014-04-12 01:39:30 -0700

    Gets like and dislike button to switch depending on the state.

--- client/js/controller.js
+++ client/js/controller.js
@@ -1,11 +1,11 @@
 Meteor.startup(function () {
-	Session.set('current_page', 'top');
+	Session.set('current_page', 'latest');
 	$('#latest').css('font-size', '22px');
 });
 
 Template.post_list.posts = function (argument) {
 	switch (Session.get('current_page')) {
 		case 'top':	return Posts.find({}, {sort: {likes: -1, post_number: -1}}); break;
-		case 'latest': return Posts.find({}, {sort: {timestamp: -1, post_number: -1}}); break;
+		case 'latest': return Posts.find({}, {sort: {post_number: -1, post_number: -1}}); break;
 	}
 };
--- client/js/helpers.js
+++ client/js/helpers.js
@@ -1,8 +1,19 @@
-/*Template.post.has_liked = function () {
-	debugger;
-	if (_.contains(this.was_liked_by, Meteor.user()._id)) {
-		return 'block';
+Template.post.has_liked = function () {
+	if (!_.contains(this.was_liked_by, Meteor.userId())) {
+		return 'inline-block';
 	} else {
 		return 'none';
 	}
-};*/
+};
+
+Template.post.has_unliked = function () {
+	if (_.contains(this.was_liked_by, Meteor.userId())) {
+		return 'inline-block';
+	} else {
+		return 'none';
+	}
+};
+
+/*Template.post.rendered = function () {
+	if (_.comment_details(this.data.was_liked_by))
+}*/

12362f41ceb65ef939b8a76eb3ea86b4f4c4ec78
Matt Dahl <matt.dahl.2013@gmail.com>
2014-04-11 22:22:11 -0700

    Enables basic post submission.

--- client/js/events.js
+++ client/js/events.js
@@ -6,6 +6,16 @@ Template.header.events = {
 
 Template.submit_dialog.events = {
 	'click div.submit': function (event, template) {
+		Posts.insert({
+			post_number: Posts.find().count(),
+			body: template.find('#submission_body').value,
+			is_approved: false,
+			was_liked_by: [],
+			timestamp: (new Date()).toString().substring(0, 21),
+			comments: [],
+			content_warnings: []
+		});
+
 		$('#submit_dialog').hide();
 	}
 };

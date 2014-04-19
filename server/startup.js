ffd838f3905a67e5d0e1515a1ac7ace821a8fd0d
Matt Dahl <matt.dahl.2013@gmail.com>
2014-04-18 21:07:16 -0700

    Adds new admin credentials for Heroku deploy.

--- server/startup.js
+++ server/startup.js
@@ -1,7 +1,6 @@
 Meteor.startup(function () {
 	// Set up admin credentials
-	Meteor.users.update('ovu5ou6mxB8LjLmMM', {$set: {is_admin: true}});
-	Meteor.users.update('fhDvGjhhrESaKxvs8', {$set: {is_admin: true}});
+	Meteor.users.update('okPjs3HjPwMRb2Ghk', {$set: {is_admin: true}});
 
 	if (Posts.find().count() < 100) {
 		load_data();
